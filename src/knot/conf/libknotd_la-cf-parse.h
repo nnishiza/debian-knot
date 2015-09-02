3eb127c80c8c13ef8bc723c88d0f97814ed0eba8
Ondřej Surý <ondrej@sury.org>
2015-09-02 10:49:38 +0200

    Imported Upstream version 1.6.5

--- src/knot/conf/libknotd_la-cf-parse.h
+++ src/knot/conf/libknotd_la-cf-parse.h
@@ -61,57 +61,58 @@ extern int cf_debug;
     KEY = 271,
     KEYS = 272,
     MAX_UDP_PAYLOAD = 273,
-    TSIG_ALGO_NAME = 274,
-    WORKERS = 275,
-    BACKGROUND_WORKERS = 276,
-    ASYNC_START = 277,
-    USER = 278,
-    RUNDIR = 279,
-    PIDFILE = 280,
-    REMOTES = 281,
-    GROUPS = 282,
-    ZONES = 283,
-    FILENAME = 284,
-    DISABLE_ANY = 285,
-    SEMANTIC_CHECKS = 286,
-    NOTIFY_RETRIES = 287,
-    NOTIFY_TIMEOUT = 288,
-    DBSYNC_TIMEOUT = 289,
-    IXFR_FSLIMIT = 290,
-    XFR_IN = 291,
-    XFR_OUT = 292,
-    UPDATE_IN = 293,
-    NOTIFY_IN = 294,
-    NOTIFY_OUT = 295,
-    BUILD_DIFFS = 296,
-    MAX_CONN_IDLE = 297,
-    MAX_CONN_HS = 298,
-    MAX_CONN_REPLY = 299,
-    MAX_TCP_CLIENTS = 300,
-    RATE_LIMIT = 301,
-    RATE_LIMIT_SIZE = 302,
-    RATE_LIMIT_SLIP = 303,
-    TRANSFERS = 304,
-    STORAGE = 305,
-    DNSSEC_ENABLE = 306,
-    DNSSEC_KEYDIR = 307,
-    SIGNATURE_LIFETIME = 308,
-    SERIAL_POLICY = 309,
-    SERIAL_POLICY_VAL = 310,
-    QUERY_MODULE = 311,
-    INTERFACES = 312,
-    ADDRESS = 313,
-    PORT = 314,
-    IPA = 315,
-    IPA6 = 316,
-    VIA = 317,
-    CONTROL = 318,
-    ALLOW = 319,
-    LISTEN_ON = 320,
-    LOG = 321,
-    LOG_DEST = 322,
-    LOG_SRC = 323,
-    LOG_LEVEL = 324
+    REQ_EDNS_OPT = 274,
+    TSIG_ALGO_NAME = 275,
+    WORKERS = 276,
+    BACKGROUND_WORKERS = 277,
+    ASYNC_START = 278,
+    USER = 279,
+    RUNDIR = 280,
+    PIDFILE = 281,
+    REMOTES = 282,
+    GROUPS = 283,
+    ZONES = 284,
+    FILENAME = 285,
+    DISABLE_ANY = 286,
+    SEMANTIC_CHECKS = 287,
+    NOTIFY_RETRIES = 288,
+    NOTIFY_TIMEOUT = 289,
+    DBSYNC_TIMEOUT = 290,
+    IXFR_FSLIMIT = 291,
+    XFR_IN = 292,
+    XFR_OUT = 293,
+    UPDATE_IN = 294,
+    NOTIFY_IN = 295,
+    NOTIFY_OUT = 296,
+    BUILD_DIFFS = 297,
+    MAX_CONN_IDLE = 298,
+    MAX_CONN_HS = 299,
+    MAX_CONN_REPLY = 300,
+    MAX_TCP_CLIENTS = 301,
+    RATE_LIMIT = 302,
+    RATE_LIMIT_SIZE = 303,
+    RATE_LIMIT_SLIP = 304,
+    TRANSFERS = 305,
+    STORAGE = 306,
+    DNSSEC_ENABLE = 307,
+    DNSSEC_KEYDIR = 308,
+    SIGNATURE_LIFETIME = 309,
+    SERIAL_POLICY = 310,
+    SERIAL_POLICY_VAL = 311,
+    QUERY_MODULE = 312,
+    INTERFACES = 313,
+    ADDRESS = 314,
+    PORT = 315,
+    IPA = 316,
+    IPA6 = 317,
+    VIA = 318,
+    CONTROL = 319,
+    ALLOW = 320,
+    LISTEN_ON = 321,
+    LOG = 322,
+    LOG_DEST = 323,
+    LOG_SRC = 324,
+    LOG_LEVEL = 325
   };
 #endif
 /* Tokens.  */
@@ -131,57 +132,58 @@ extern int cf_debug;
 #define KEY 271
 #define KEYS 272
 #define MAX_UDP_PAYLOAD 273
-#define TSIG_ALGO_NAME 274
-#define WORKERS 275
-#define BACKGROUND_WORKERS 276
-#define ASYNC_START 277
-#define USER 278
-#define RUNDIR 279
-#define PIDFILE 280
-#define REMOTES 281
-#define GROUPS 282
-#define ZONES 283
-#define FILENAME 284
-#define DISABLE_ANY 285
-#define SEMANTIC_CHECKS 286
-#define NOTIFY_RETRIES 287
-#define NOTIFY_TIMEOUT 288
-#define DBSYNC_TIMEOUT 289
-#define IXFR_FSLIMIT 290
-#define XFR_IN 291
-#define XFR_OUT 292
-#define UPDATE_IN 293
-#define NOTIFY_IN 294
-#define NOTIFY_OUT 295
-#define BUILD_DIFFS 296
-#define MAX_CONN_IDLE 297
-#define MAX_CONN_HS 298
-#define MAX_CONN_REPLY 299
-#define MAX_TCP_CLIENTS 300
-#define RATE_LIMIT 301
-#define RATE_LIMIT_SIZE 302
-#define RATE_LIMIT_SLIP 303
-#define TRANSFERS 304
-#define STORAGE 305
-#define DNSSEC_ENABLE 306
-#define DNSSEC_KEYDIR 307
-#define SIGNATURE_LIFETIME 308
-#define SERIAL_POLICY 309
-#define SERIAL_POLICY_VAL 310
-#define QUERY_MODULE 311
-#define INTERFACES 312
-#define ADDRESS 313
-#define PORT 314
-#define IPA 315
-#define IPA6 316
-#define VIA 317
-#define CONTROL 318
-#define ALLOW 319
-#define LISTEN_ON 320
-#define LOG 321
-#define LOG_DEST 322
-#define LOG_SRC 323
-#define LOG_LEVEL 324
+#define REQ_EDNS_OPT 274
+#define TSIG_ALGO_NAME 275
+#define WORKERS 276
+#define BACKGROUND_WORKERS 277
+#define ASYNC_START 278
+#define USER 279
+#define RUNDIR 280
+#define PIDFILE 281
+#define REMOTES 282
+#define GROUPS 283
+#define ZONES 284
+#define FILENAME 285
+#define DISABLE_ANY 286
+#define SEMANTIC_CHECKS 287
+#define NOTIFY_RETRIES 288
+#define NOTIFY_TIMEOUT 289
+#define DBSYNC_TIMEOUT 290
+#define IXFR_FSLIMIT 291
+#define XFR_IN 292
+#define XFR_OUT 293
+#define UPDATE_IN 294
+#define NOTIFY_IN 295
+#define NOTIFY_OUT 296
+#define BUILD_DIFFS 297
+#define MAX_CONN_IDLE 298
+#define MAX_CONN_HS 299
+#define MAX_CONN_REPLY 300
+#define MAX_TCP_CLIENTS 301
+#define RATE_LIMIT 302
+#define RATE_LIMIT_SIZE 303
+#define RATE_LIMIT_SLIP 304
+#define TRANSFERS 305
+#define STORAGE 306
+#define DNSSEC_ENABLE 307
+#define DNSSEC_KEYDIR 308
+#define SIGNATURE_LIFETIME 309
+#define SERIAL_POLICY 310
+#define SERIAL_POLICY_VAL 311
+#define QUERY_MODULE 312
+#define INTERFACES 313
+#define ADDRESS 314
+#define PORT 315
+#define IPA 316
+#define IPA6 317
+#define VIA 318
+#define CONTROL 319
+#define ALLOW 320
+#define LISTEN_ON 321
+#define LOG 322
+#define LOG_DEST 323
+#define LOG_SRC 324
+#define LOG_LEVEL 325
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
@@ -197,7 +199,7 @@ union YYSTYPE
 		knot_tsig_algorithm_t alg;
 	} tok;
 
-#line 201 "knot/conf/libknotd_la-cf-parse.h" /* yacc.c:1909  */
+#line 203 "knot/conf/libknotd_la-cf-parse.h" /* yacc.c:1909  */
 };
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
